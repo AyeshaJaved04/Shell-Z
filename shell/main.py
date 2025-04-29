bab7d38a7aa5fae4cfe409f4e1e9a956cc0666f9
Ayesha Javed <aayesha.javed04@gmail.com>
2025-04-29 15:52:50 +0500

    add main.py for executing queries with Gemini, Deepseek, and ChatGPT models

---
+++ shell/main.py
@@ -0,0 +1,28 @@
+import sys
+from modules import clean_response
+from modules import typewriter_effect
+from modules import ask_gemini
+from modules import ask_chatGPT
+from modules import ask_deepseek
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) < 3:
+        print("Usage: python3 utils.py \"your prompt\" model_name")
+        sys.exit(1)
+
+    user_query = sys.argv[1]
+    model_name = sys.argv[2].lower()
+
+    if model_name == "gemini":
+        raw_response = ask_gemini(user_query)
+    elif model_name == "deepseek":
+        raw_response = ask_deepseek(user_query)
+    elif model_name == "gpt":
+        raw_response = ask_chatGPT(user_query)
+    else:
+        print(f"Shell-Z doesn't support: {model_name}")
+        sys.exit(1)
+
+    cleaned_response = clean_response(raw_response)
+    typewriter_effect(cleaned_response)
